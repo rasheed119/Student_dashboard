f390f1827259d9df88722435c8f25c8df5bc044e
rasheed119 <rasheed20052000@gmail.com>
2023-04-28 01:17:47 +0530

    1 coomit

---
+++ src/Component/Addstudents.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Addstudents({student,setstudent}){
+  const [name,setname] = useState("");
+  const [qualification,setqualification] = useState("");
+  const [gender,setgender] = useState("");
+  const [batch,setbatch] = useState("");
+
+  function add_data(){
+    const new_data = {
+      name,
+      batch,
+      qualification,
+      gender
+    };
+    setstudent([...student,new_data]);
+    setname("");
+    setbatch("")
+    setgender("");
+    setqualification("");
+  }
+
+  return(
+    <div className="addstudents">
+      <input
+      type="text"
+      placeholder="Enter name"
+      value={name}
+      onChange={(e)=>setname(e.target.value)}
+      />
+      <input
+      type="text"
+      placeholder="Enter qualification"
+      value={qualification}
+      onChange={(e)=>setqualification(e.target.value)}
+      />
+      <input
+      type="text"
+      placeholder="Enter Batch"
+      value={batch}
+      onChange={(e)=>setbatch(e.target.value)}
+      />
+      <input
+      type="text"
+      placeholder="Enter Gender"
+      value={gender}
+      onChange={(e)=>setgender(e.target.value)}
+      />
+      <Button variant="primary"
+      onClick={add_data}
+      >Add Student</Button>
+    </div>
+  );
+}
+export default Addstudents;
--- src/Component/Students.js
+++ src/Component/Students.js
@@ -1,16 +1,61 @@
 import React from "react"
 import Base from "../Base/Base";
 import data from "../Data/Data";
-import { useState } from "react"
+import Addstudents from "./Addstudents"
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default function Students(){
-  const [students , setstudents] = useState(data);
-  console.log(students);
+  const [student , setstudent] = useState(data);
+
+  const deletestudent = (studidx)=>{
+    const remainingstudent = student.filter((stdobj,idx)=> idx !== studidx);
+    setstudent(remainingstudent);
+  }
+
   return(
     <Base
     Title = {"Students Dashboard"}
     Description = {"This page contains Students data"}
     >
+
+      <div className="container">
+        <div>
+          <Addstudents
+          student = {student}
+          setstudent = {setstudent}
+          />
+        </div>
+        <div className="row">
+          
+        {student.map((stud,idx)=>(
+        <Card style={{ width: '18rem' }} key={idx}>
+      <Card.Body>
+        <Card.Text>
+          Name : {stud.name}
+        </Card.Text>
+        <Card.Text>
+          Qualification : {stud.qualification}
+        </Card.Text>
+        <Card.Text>
+          Batch : {stud.batch}
+        </Card.Text>
+        <Card.Text>
+          Gender : {stud.gender}
+        </Card.Text>
+        <div>
+        <Button variant="primary">Edit</Button>
+        <Button variant="danger"
+        onClick = {()=>deletestudent(idx)}
+        >Delete</Button>
+        </div>
+      </Card.Body>
+    </Card>
+    ))}
+        </div>
+      </div>
     </Base>
   )
 }

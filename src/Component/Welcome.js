2e8a582c116753517ae97fccbafbe0e0140a89ec
rasheed119 <rasheed20052000@gmail.com>
2023-05-01 18:00:08 +0530

    adding button instead on a tag

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 function Welcome() {
+  const history = useHistory();
   return (
     <div className='Welcome'>
       <h1>Welcome to the Student Dashboard</h1>
-      <h3><a href='/homepage'>Click Here</a> to go to Student Dashboard</h3>
+      <h3><Button onClick={()=>history.push("/homepage")}>Click Here</Button> to go to Student Dashboard</h3>
     </div>
   )
 }
